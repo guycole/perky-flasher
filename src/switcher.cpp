9972e4b24c8ece55acb82d77ade79ae65f407e57
Guy Cole <guycole@gmail.com>
2016-03-19 21:53:43 -0700

    in progress

--- src/switcher.cpp
+++ src/switcher.cpp
@@ -24,6 +24,22 @@
 
 //using namespace std;
 
+enum run_mode { sequence, fault };
+
+class Configuration {
+public:
+  void setRunMode(run_mode next_mode);
+  run_mode getRunMode(){return current_mode;}
+  int getSwitchGpio(){return switch_gpio;}
+private:
+  int switch_gpio;
+  run_mode current_mode;
+};
+
+void Configuration::setRunMode(run_mode next_mode) {
+  current_mode = next_mode;
+}
+  
 int main(int argc, char *argv[]) {
   printf("fix me\n");
 }
