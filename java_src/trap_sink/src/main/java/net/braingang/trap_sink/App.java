6886fae884a24e65e48ebbc3ecc5dbbb1d3c3428
Guy Cole <guycole@gmail.com>
2016-05-21 12:08:01 -0700

    now in java

---
+++ java_src/trap_sink/src/main/java/net/braingang/trap_sink/App.java
@@ -0,0 +1,13 @@
+package net.braingang.trap_sink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
